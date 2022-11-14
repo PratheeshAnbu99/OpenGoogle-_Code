b677fedf2e45c81bc8174541da061f91c1939c69
PratheeshAnbu99 <117888815+PratheeshAnbu99@users.noreply.github.com>
2022-11-14 16:08:19 +0530

    Initial Committ

---
+++ OpenGoogle.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Jars\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new 	ChromeDriver();
+		driver.get("http:\\www.google.co.in");
+		
+		
+
+	}
+
+}
